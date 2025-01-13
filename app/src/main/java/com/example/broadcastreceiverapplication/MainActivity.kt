b03f0da96210c20e3c0d802bfa0e3c8376745302
prativa neupane <prativananu123@gmail.com>
2025-01-13 10:06:25 +0545

    add runnable

--- app/src/main/java/com/example/broadcastreceiverapplication/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverapplication/MainActivity.kt
@@ -40,6 +40,27 @@ class MainActivity : AppCompatActivity() {
 
         backgroundThread.start()
 
+
+        // Create a Runnable for the background task
+        val backgroundRunnable = Runnable {
+            // Simulate a time-consuming task (e.g., network request or heavy computation)
+            Thread.sleep(2000)  // 2-second delay to simulate background work
+
+            // Task result after background work
+            val result = "Task Completed from runnable"
+
+            // Now update the UI on the main thread
+            val mainHandler = Handler(Looper.getMainLooper())
+            mainHandler.post {
+                statusTextView.text = result  // Update the TextView with the result
+            }
+        }
+
+        // Create and start a background thread to run the Runnable
+        val backgroundThreadFromRunnnnable = Thread(backgroundRunnable)
+        backgroundThreadFromRunnnnable.start()  // Start the background thread
+
+
     }
 
     override fun onDestroy() {
